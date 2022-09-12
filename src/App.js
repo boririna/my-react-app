ba0effac7ef47a50e604564ce16e162fd4f25feb
Irina Borisova <i.a.borisova@gmail.com>
2022-09-12 22:13:06 +0200

    PrevEvents, props, conditions, multiple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Title from './components/Title'
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState(
+    [
+      {title: "some event", id: 1},
+      {title: "some other event", id: 2},
+      {title: "one more event", id: 3}
+    ])
+
+  console.log(showEvents)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
+  }
+  const sub = "Some subtitle"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Some title" subtitle={sub}/>
+      <Title title="Another title" subtitle="Another subtitle"/>
+
+      {showEvents && (<div>
+        <button onClick={() => setShowEvents(false)}>Hide events</button>
+      </div>)}
+      {!showEvents && (<div>
+        <button onClick={() => setShowEvents(true)}>Show events</button>
+      </div>)}
+      {showEvents && events.map((event, index) =>(
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
